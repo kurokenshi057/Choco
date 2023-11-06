b536931caccb811b928b061fe2f84046f9b9557f
kurokenshi057 <116281080+kurokenshi057@users.noreply.github.com>
2023-11-06 11:30:54 +0100

    Add files via upload

---
+++ choco version final/models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace choco.models
+{
+    public class Article
+    {
+        public Guid Id { get; set; }
+        public string Reference { get; set; }
+        public float Prix { get; set; }
+    }
+
+}

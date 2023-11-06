b58fd36f727a04b233be5d2c44ee06f88c5feb68
Nicolas Flauder <116281080+kurokenshi057@users.noreply.github.com>
2023-11-06 13:49:28 +0100

    Add files via upload

---
+++ choco version finale/models/ArticleAchete.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace choco.models
+{
+    public class ArticleAchete
+    {
+        public Guid IdAcheteur { get; set; }
+        public Guid IdArticle { get; set; }
+        public int Quantite { get; set; }
+        public DateTime DateAchat { get; set; }
+    }
+
+}
